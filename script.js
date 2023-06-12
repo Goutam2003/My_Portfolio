edc771e8e646e212fb26988b358c7fd11c3f0f77
Goutam Parmar <118896899+Goutam2003@users.noreply.github.com>
2023-06-12 21:45:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Portfolio."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
